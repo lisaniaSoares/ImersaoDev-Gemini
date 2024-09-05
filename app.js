9b338dcbc04cc486828ca42c1b8b9ab02e88108d
lisaniaSoares <lisasoares.if@gmail.com>
2024-09-05 17:21:49 -0300

    Quarto dia de Imersão dev web

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 //Código para o lógica de Java
 
-console.log(cientistas[0].nome);
+//console.log(cientistas.nome);
+
+//Função JS
+function pesquisar(){
+
+    //Referenciando uma section do HTML pelo seu id 
+    let section = document.getElementById("resultados-pesquisa");  
+
+    //Iniciando uma variável que guardará todo o conteúdo que será enviado para o HTML
+    let resultados = "";
+
+    //Visando conseguir atribuir à tela todas as informções da lista de dados, faremos um for
+    for(let dado of cientistas){
+    
+        //Somamos, para que as informações, ao invés de se sobreporem, elas se adicionem
+        resultados += `
+        <div class="item-resultado"> 
+            <h2>
+                <a href="#">${dado.nome}</a>
+            </h2>
+            <h5 class="descricao-meta">
+                <a href="#">${dado.nascimento} - ${dado.morte} </a>
+            </h5>
+            <p class="descricao-meta">${dado.descricao} </p>
+            <p class="descricao-meta">
+                <a href="#">Área de atuação: ${dado.area}</a> 
+            </p>
+            <a href=${dado.wiki} target="_blank">Mais informações sobre ela.</a>
+        </div>
+        `;
+    }
+
+    //O innerHTML serve para definir o conteúdo HTML interno do elemento, que será toda a nosaa variável resultado
+    section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -26,72 +26,72 @@ let cientistas = [
         nome: "Marie Curie",
         descricao: "Marie Curie foi uma física e química polonesa, naturalizada francesa, pioneira no estudo da radioatividade. Ela foi a primeira mulher a ganhar um Prêmio Nobel, e a única a ganhar em duas áreas científicas diferentes: Física e Química.",
         area: "Física e Química",
-        nascimento: "1867-11-07",
-        morte: "1934-07-04",
+        nascimento: "1867",
+        morte: "1934",
         wiki: "https://pt.wikipedia.org/wiki/Marie_Curie"
     },
     {
         nome: "Rosalind Franklin",
         descricao: "Cristalografa britânica, Rosalind Franklin produziu a famosa 'Foto 51', uma imagem por difração de raios X que revelou a estrutura helicoidal do DNA. Seu trabalho foi fundamental para a compreensão da molécula da vida, embora seu papel tenha sido subestimado em vida.",
         area: "Biofísica",
-        nascimento: "1920-07-25",
-        morte: "1958-04-16",
+        nascimento: "1920",
+        morte: "1958",
         wiki: "https://pt.wikipedia.org/wiki/Rosalind_Franklin"
     },
     {
         nome: "Ada Lovelace",
         descricao: "Ada Lovelace, nascida Augusta Ada Byron, foi uma matemática e escritora inglesa que revolucionou o mundo da computação. Considerada a primeira programadora da história, ela trabalhou com a Máquina Analítica de Charles Babbage, antecessora dos computadores modernos.",
         area: "Matemática",
-        nascimento: "1815-12-10",
-        morte: "1852-11-27",
+        nascimento: "1815",
+        morte: "1852",
         wiki: "https://pt.wikipedia.org/wiki/Ada_Lovelace"
     },
     {
         nome: "Elizabeth Blackwell",
         descricao: "Elizabeth Blackwell foi a primeira mulher a se formar em medicina nos Estados Unidos. Ela enfrentou inúmeros obstáculos para alcançar seu objetivo e se tornou uma defensora dos direitos das mulheres na área da saúde.",
         area: "Medicina",
-        nascimento: "1821-02-03",
-        morte: "1910-05-31",
+        nascimento: "1821",
+        morte: "1910",
         wiki: "https://pt.wikipedia.org/wiki/Elizabeth_Blackwell"
     },
     {
         nome: "Katherine Clerk Maxwell",
         descricao: "Katherine Clerk Maxwell foi uma física teórica que fez contribuições significativas para a teoria eletromagnética. Ela trabalhou em estreita colaboração com seu marido, James Clerk Maxwell, um dos cientistas mais importantes do século XIX.",
         area: "Física e Matemática",
-        nascimento: "1831-06-13",
-        morte: "1907-04-05",
+        nascimento: "1831",
+        morte: "1907",
         wiki: "https://pt.wikipedia.org/wiki/Katherine_Clerk_Maxwell"
     },
     {
         nome: "Mary Anning",
         descricao: "Mary Anning foi uma paleontóloga amadora que fez importantes descobertas de fósseis de dinossauros. Suas descobertas contribuíram significativamente para a compreensão da vida pré-histórica.",
         area: "Paleontologia",
-        nascimento: "1799-05-21",
-        morte: "1847-03-9",
+        nascimento: "1799",
+        morte: "1847",
         wiki: "https://pt.wikipedia.org/wiki/Mary_Anning"
     },
     {
         nome: "Grace Hopper",
         descricao: "Almirante da Marinha dos Estados Unidos e pioneira em ciência da computação, Grace Hopper desenvolveu o primeiro compilador e popularizou a linguagem de programação COBOL. Seus trabalhos foram fundamentais para a criação de softwares mais acessíveis e eficientes.",
         area: "Ciência da Computação",
-        nascimento: "1906-12-09",
-        morte: "1992-01-01",
+        nascimento: "1906",
+        morte: "1992",
         wiki: "https://pt.wikipedia.org/wiki/Grace_Hopper"
     },
     {
         nome: "Lise Meitner",
         descricao: "Física austríaca, Lise Meitner foi uma das principais cientistas envolvidas na descoberta da fissão nuclear. Seus trabalhos teóricos foram fundamentais para a compreensão desse fenômeno e suas implicações para a energia nuclear.",
         area: "Física Nuclear",
-        nascimento: "1878-11-07",
-        morte: "1968-10-27",
+        nascimento: "1878",
+        morte: "1968",
         wiki: "https://pt.wikipedia.org/wiki/Lise_Meitner"
     },
     {
         nome: "Frances Allen",
         descricao: "Cientista da computação americana, Frances Allen foi a primeira mulher a ganhar o Prêmio Turing, considerado o Nobel da computação. Seus trabalhos pioneiros em otimização de compiladores e análise de programas tiveram um impacto profundo na indústria de software.",
         area: "Ciência da Computação",
-        nascimento: "1932-08-04",
-        morte: "2020-08-04",
+        nascimento: "1932",
+        morte: "2020",
         wiki: "https://en.wikipedia.org/wiki/Frances_Allen" // Ainda não há página na Wikipédia em português
     }
 ];
