d9ea87c9b594ca73272ba19141c5748bc348f35c
lisaniaSoares <lisasoares.if@gmail.com>
2024-09-07 01:43:18 +0100

    Finalização - Projeto Final Imersão dev web

--- app.js
+++ app.js
@@ -13,7 +13,16 @@ function pesquisar(){
 
     //Verifica se o campo está vazio
     if(!campoPesquisa){
-        section.innerHTML = '<p> Busca vazia, pesquise alguma informação! </p>';
+        section.innerHTML = `
+            <div class="item-erro"> 
+                <p>
+                    Busca vazia, pesquise alguma informação!
+                </p>
+            </div>
+            <div>
+                <img class="img-aviso" src="img/aviso-1.png" alt="Menina cientista.">
+            </div>
+            `;
         return
     }
 
@@ -24,32 +33,40 @@ function pesquisar(){
     let nome = "";
     let descricao = "";
     let area = "";
+    let tags = "";
 
     //Visando conseguir atribuir à tela todas as informções da lista de dados, faremos um for
     for(let dado of cientistas){
         nome = dado.nome.toLowerCase();
         descricao = dado.descricao.toLowerCase();
         area = dado.area.toLowerCase();
+        tags = dado.tags.toLocaleLowerCase();
         
         //Verifica se algum item na base de dados corresponde com o que foi pesquisado
-        if(nome.includes(campoPesquisa) || descricao.includes(campoPesquisa) || area.includes(campoPesquisa)){
+        if(nome.includes(campoPesquisa) || descricao.includes(campoPesquisa) || area.includes(campoPesquisa) || tags.includes(campoPesquisa)){
 
             console.log(campoPesquisa);
 
             //Somamos, para que as informações, ao invés de se sobreporem, elas se adicionem
             resultados += `
-            <div class="item-resultado"> 
-                <h2>
-                    <a href="#">${dado.nome}</a>
-                </h2>
-                <h5 class="descricao-meta">
-                    <a href="#">${dado.nascimento} - ${dado.morte} </a>
-                </h5>
-                <p class="descricao-meta">${dado.descricao} </p>
-                <p class="descricao-meta">
-                    <a href="#">Área de atuação: ${dado.area}</a> 
-                </p>
-                <a href=${dado.wiki} target="_blank">Mais informações sobre ela.</a>
+
+            <div class="item-resultado">
+                <div>
+                    <img class="img-resultado" src="${dado.img}" alt="${dado.nome}">
+                </div>
+                <div> 
+                    <h2>
+                        <a href="#">${dado.nome}</a>
+                    </h2>
+                    <h4 class="descricao-meta">
+                        <a href="#">${dado.nascimento} - ${dado.morte} </a>
+                    </h4>
+                    <p class="descricao-meta">${dado.descricao} </p>
+                    <p class="descricao-meta">
+                        <a href="#">Área de atuação: ${dado.area}</a> 
+                    </p>
+                    <a href=${dado.wiki} target="_blank">Mais informações sobre ela.</a>
+                </div>
             </div>
             `;
         }
@@ -57,7 +74,16 @@ function pesquisar(){
     }
 
     if(!resultados){
-        resultados = "<p> Não temos essa informação em nossa base de dados, desculpe.</p>";
+        resultados = `
+            <div class="item-erro"> 
+                <p>
+                    Não temos essa informação em nossa base de dados. Desculpe!
+                </p>
+            </div>
+            <div>
+                <img class="img-aviso" src="img/aviso-2.png" alt="Menina cientista.">
+            </div>
+        `;
     }
 
     //O innerHTML serve para definir o conteúdo HTML interno do elemento, que será toda a nosaa variável resultado
--- dados.js
+++ dados.js
@@ -28,7 +28,9 @@ let cientistas = [
         area: "Física e Química",
         nascimento: "1867",
         morte: "1934",
-        wiki: "https://pt.wikipedia.org/wiki/Marie_Curie"
+        wiki: "https://pt.wikipedia.org/wiki/Marie_Curie",
+        img: "img/cientistas/marie-curie.jpeg",
+        tags: "quimica química física fisica radioatividade marrie currie"
     },
     {
         nome: "Rosalind Franklin",
@@ -36,7 +38,9 @@ let cientistas = [
         area: "Biofísica",
         nascimento: "1920",
         morte: "1958",
-        wiki: "https://pt.wikipedia.org/wiki/Rosalind_Franklin"
+        wiki: "https://pt.wikipedia.org/wiki/Rosalind_Franklin",
+        img: "img/cientistas/rosalind-franklin.jpg",
+        tags: "fotografia biologia fisica física molecula DNA dna rna"
     },
     {
         nome: "Ada Lovelace",
@@ -44,7 +48,9 @@ let cientistas = [
         area: "Matemática",
         nascimento: "1815",
         morte: "1852",
-        wiki: "https://pt.wikipedia.org/wiki/Ada_Lovelace"
+        wiki: "https://pt.wikipedia.org/wiki/Ada_Lovelace",
+        img: "img/cientistas/ada-lovelace.webp",
+        tags: "programação programaçao programacao computaçao computacao programadora analise maquina matematica computacão tech tecnologia tecnologica tecnológica"
     },
     {
         nome: "Elizabeth Blackwell",
@@ -52,7 +58,9 @@ let cientistas = [
         area: "Medicina",
         nascimento: "1821",
         morte: "1910",
-        wiki: "https://pt.wikipedia.org/wiki/Elizabeth_Blackwell"
+        wiki: "https://pt.wikipedia.org/wiki/Elizabeth_Blackwell",
+        img: "img/cientistas/elizabeth-blackwell.jpg",
+        tags: "medicina medica médica saude"
     },
     {
         nome: "Katherine Clerk Maxwell",
@@ -60,7 +68,9 @@ let cientistas = [
         area: "Física e Matemática",
         nascimento: "1831",
         morte: "1907",
-        wiki: "https://pt.wikipedia.org/wiki/Katherine_Clerk_Maxwell"
+        wiki: "https://pt.wikipedia.org/wiki/Katherine_Clerk_Maxwell",
+        img: "img/cientistas/katherine-clerk.jpeg",
+        tags: "fisica matematica matemática teorica eletromagnetismo eletron elétron eletromagnetica"
     },
     {
         nome: "Mary Anning",
@@ -68,7 +78,9 @@ let cientistas = [
         area: "Paleontologia",
         nascimento: "1799",
         morte: "1847",
-        wiki: "https://pt.wikipedia.org/wiki/Mary_Anning"
+        wiki: "https://pt.wikipedia.org/wiki/Mary_Anning",
+        img: "img/cientistas/mary-anning.jpg",
+        tags: "fossil fosseis paleontologa pre historia"
     },
     {
         nome: "Grace Hopper",
@@ -76,7 +88,9 @@ let cientistas = [
         area: "Ciência da Computação",
         nascimento: "1906",
         morte: "1992",
-        wiki: "https://pt.wikipedia.org/wiki/Grace_Hopper"
+        wiki: "https://pt.wikipedia.org/wiki/Grace_Hopper",
+        img: "img/cientistas/grace-hopper.jpg",
+        tags: "ciencia computacao computacão computaçao programação programaçao programacao robotica robô robótica lingua tech tecnologia tecnologica tecnológica"
     },
     {
         nome: "Lise Meitner",
@@ -84,7 +98,9 @@ let cientistas = [
         area: "Física Nuclear",
         nascimento: "1878",
         morte: "1968",
-        wiki: "https://pt.wikipedia.org/wiki/Lise_Meitner"
+        wiki: "https://pt.wikipedia.org/wiki/Lise_Meitner",
+        img: "img/cientistas/lise.jpeg",
+        tags: "fisica nucleo fissao teorica"
     },
     {
         nome: "Frances Allen",
@@ -92,7 +108,79 @@ let cientistas = [
         area: "Ciência da Computação",
         nascimento: "1932",
         morte: "2020",
-        wiki: "https://en.wikipedia.org/wiki/Frances_Allen" // Ainda não há página na Wikipédia em português
+        wiki: "https://en.wikipedia.org/wiki/Frances_Allen", // Ainda não há página na Wikipédia em português
+        img: "img/cientistas/frances-allen.jpg",
+        tags: "ciencia computacao computacão computaçao programação programaçao programacao robotica robô robótica lingua industria premio tech tecnologia tecnologica tecnológica"
+    },
+    {
+        nome: "Chien-Shiung Wu",
+        descricao: "Física experimental chinesa que realizou experimentos cruciais que levaram à refutação da lei de conservação da paridade, um princípio fundamental da física.",
+        area: "Física",
+        nascimento: "1912",
+        morte: "1997",
+        wiki: "https://pt.wikipedia.org/wiki/Chien-Shiung_Wu",
+        img: "img/cientistas/chien-shiung.jpg",
+        tags: "fisica conservacao conservacão principio"
+    },
+    {
+        nome: "Gladys West",
+        descricao: "Matemática americana que contribuiu significativamente para o desenvolvimento do sistema de posicionamento global (GPS). Seus cálculos precisos e meticulosos foram essenciais para a criação de um modelo matemático da Terra, que serviu como base para o funcionamento do GPS.",
+        area: "Matemática",
+        nascimento: "1930",
+        morte: "Dias atuais",
+        wiki: "https://pt.wikipedia.org/wiki/Gladys_West",
+        img: "img/cientistas/gladys-west.jpeg",
+        tags: "matematica calculo matematico tech tecnologia tecnologica tecnológica"
+    },
+    {
+        nome: "Jaqueline Goes de Jesus",
+        descricao: "Bióloga molecular brasileira que liderou a equipe que sequenciou o genoma do vírus SARS-CoV-2 no Brasil em tempo recorde. Seu trabalho foi fundamental para o desenvolvimento de testes e vacinas contra a COVID-19 e a compreensão da evolução do vírus.",
+        area: "Biologia Molecular",
+        nascimento: "1989", 
+        morte: "Dias atuais",
+        wiki: "https://www.uol.com.br/universa/noticias/redacao/2020/05/14/jaqueline-goes-de-jesus-a-cientista-que-desvendou-o-coronavirus-no-brasil.htm",
+        img: "img/cientistas/jaqueline-goes.png",
+        tags: "virus saude saúde covid biologa biologica rna"
+    },
+    {
+        nome: "Katherine Johnson",
+        descricao: " Foi uma matemática, física e cientista espacial norte-americana de renome mundial. Seus cálculos precisos foram cruciais para o sucesso das primeiras missões espaciais dos Estados Unidos, incluindo os projetos Mercury, Gemini e Apollo, realizou cálculos complexos para as missões espaciais da NASA.",
+        area: "Matemática",
+        nascimento: "1928",
+        morte: "Dias atuais",
+        wiki: "https://pt.wikipedia.org/wiki/Katherine_Johnson",
+        img: "img/cientistas/katherine-jhonson.jpg",
+        tags: "calculo calculos fisica matematica missoes EUA"
+    },
+    {
+        nome: "Jennifer Anne Doudna",
+        descricao: " Bioquímica americana, pioneira na técnica de edição genética CRISPR-Cas9. Seus trabalhos revolucionários na área de biologia molecular abriram novas possibilidades para a cura de doenças genéticas e a engenharia genética.",
+        area: "Bioquímica e Genética",
+        nascimento: "1964",
+        morte: "Dias atuais",
+        wiki: "https://pt.wikipedia.org/wiki/Jennifer_Doudna",
+        img: "img/cientistas/jennifer-anne.jpg",
+        tags: "biologia quimica bioquimica química genetica"
+    },
+    {
+        nome: "Joy Buolamwini",
+        descricao: "Cientista da computação e ativista digital ganense-americana. Buolamwini é conhecida por seu trabalho pioneiro na detecção de vieses algorítmicos em sistemas de reconhecimento facial, especialmente em relação a pessoas negras e mulheres.",
+        area: "Ciência da computação e Inteligência Artificial",
+        nascimento: "1989",
+        morte: "Dias atuais",
+        wiki: "https://en.wikipedia.org/wiki/Joy_Buolamwini",
+        img: "img/cientistas/joy-buolamwini.jpeg",
+        tags: "ciencia computacao computacão computaçao programação programaçao programacao tech tecnologia tecnologica tecnológica ia ai deteccao deteccão inteligencia"
+    },
+    {
+        nome: "Carol Shaw",
+        descricao: "Pioneira na indústria de videogames, programadora e designer de jogos.Ela é frequentemente reconhecida como a primeira programadora de jogos profissional e sua contribuição mais famosa é o clássico River Raid para o Atari 2600.",
+        area: "Desenvolvimento de Jogos Eletrônicos",
+        nascimento: "1955",
+        morte: "Dias atuais",
+        wiki: "https://movimentomulheresnati.labbs.com.br/carol-shaw/",
+        img: "img/cientistas/carol-shaw.jpg",
+        tags: "computacao computacão computaçao programação programaçao programacao tech tecnologia tecnologica tecnológica eletronico jogo videogame game video industria desenvolvedora"
     }
 ];
 
