129aa6bfd3dd2ec87ebdb3817d0cd5d948bc4e51
lisaniaSoares <lisasoares.if@gmail.com>
2024-09-11 13:28:25 -0300

    Atualização da Base de Dados

--- dados.js
+++ dados.js
@@ -30,7 +30,7 @@ let cientistas = [
         morte: "1934",
         wiki: "https://pt.wikipedia.org/wiki/Marie_Curie",
         img: "img/cientistas/marie-curie.jpeg",
-        tags: "quimica química física fisica radioatividade marrie currie"
+        tags: "fisica quimica quimica fisica química fisica radioatividade marrie currie raios-x fisica-quimica"
     },
     {
         nome: "Rosalind Franklin",
@@ -40,7 +40,7 @@ let cientistas = [
         morte: "1958",
         wiki: "https://pt.wikipedia.org/wiki/Rosalind_Franklin",
         img: "img/cientistas/rosalind-franklin.jpg",
-        tags: "fotografia biologia fisica física molecula DNA dna rna"
+        tags: "fotografia biologia fisica física molecula DNA dna rna cristalografia"
     },
     {
         nome: "Ada Lovelace",
@@ -50,7 +50,7 @@ let cientistas = [
         morte: "1852",
         wiki: "https://pt.wikipedia.org/wiki/Ada_Lovelace",
         img: "img/cientistas/ada-lovelace.webp",
-        tags: "programação programaçao programacao computaçao computacao programadora analise maquina matematica computacão tech tecnologia tecnologica tecnológica"
+        tags: "programação programaçao programacao computaçao computacao programadora analise maquina analitica matematica computacão tech tecnologia tecnologica tecnológica ada lovalece robotica robótica robô robo"
     },
     {
         nome: "Elizabeth Blackwell",
@@ -60,7 +60,7 @@ let cientistas = [
         morte: "1910",
         wiki: "https://pt.wikipedia.org/wiki/Elizabeth_Blackwell",
         img: "img/cientistas/elizabeth-blackwell.jpg",
-        tags: "medicina medica médica saude"
+        tags: "medicina medica médica saude saude feminina biologia saúde saúde feminina"
     },
     {
         nome: "Katherine Clerk Maxwell",
@@ -70,7 +70,7 @@ let cientistas = [
         morte: "1907",
         wiki: "https://pt.wikipedia.org/wiki/Katherine_Clerk_Maxwell",
         img: "img/cientistas/katherine-clerk.jpeg",
-        tags: "fisica matematica matemática teorica eletromagnetismo eletron elétron eletromagnetica"
+        tags: "fisica matematica matemática teorica teoria eletromagnetismo eletron elétron eletromagnetica katerine clerk"
     },
     {
         nome: "Mary Anning",
@@ -80,7 +80,7 @@ let cientistas = [
         morte: "1847",
         wiki: "https://pt.wikipedia.org/wiki/Mary_Anning",
         img: "img/cientistas/mary-anning.jpg",
-        tags: "fossil fosseis paleontologa pre historia"
+        tags: "fossil fosseis paleontologa pre historia história historica"
     },
     {
         nome: "Grace Hopper",
@@ -100,7 +100,7 @@ let cientistas = [
         morte: "1968",
         wiki: "https://pt.wikipedia.org/wiki/Lise_Meitner",
         img: "img/cientistas/lise.jpeg",
-        tags: "fisica nucleo fissao teorica"
+        tags: "fisica nucleo fissao teorica ciência fisica nuclear"
     },
     {
         nome: "Frances Allen",
@@ -120,7 +120,7 @@ let cientistas = [
         morte: "1997",
         wiki: "https://pt.wikipedia.org/wiki/Chien-Shiung_Wu",
         img: "img/cientistas/chien-shiung.jpg",
-        tags: "fisica conservacao conservacão principio"
+        tags: "fisica conservacao conservacão principio fisica experimental"
     },
     {
         nome: "Gladys West",
@@ -130,7 +130,7 @@ let cientistas = [
         morte: "Dias atuais",
         wiki: "https://pt.wikipedia.org/wiki/Gladys_West",
         img: "img/cientistas/gladys-west.jpeg",
-        tags: "matematica calculo matematico tech tecnologia tecnologica tecnológica"
+        tags: "matematica calculo cauculos matematico tech tecnologia tecnologica tecnológica"
     },
     {
         nome: "Jaqueline Goes de Jesus",
@@ -140,7 +140,7 @@ let cientistas = [
         morte: "Dias atuais",
         wiki: "https://www.uol.com.br/universa/noticias/redacao/2020/05/14/jaqueline-goes-de-jesus-a-cientista-que-desvendou-o-coronavirus-no-brasil.htm",
         img: "img/cientistas/jaqueline-goes.png",
-        tags: "virus saude saúde covid biologa biologica rna"
+        tags: "virus saude saúde covid biologa biologica rna brasil pandemia"
     },
     {
         nome: "Katherine Johnson",
@@ -150,7 +150,7 @@ let cientistas = [
         morte: "Dias atuais",
         wiki: "https://pt.wikipedia.org/wiki/Katherine_Johnson",
         img: "img/cientistas/katherine-jhonson.jpg",
-        tags: "calculo calculos fisica matematica missoes EUA"
+        tags: "calculo calculos fisica matematica missoes espaço espaco EUA katerine jonson katerine johnson"
     },
     {
         nome: "Jennifer Anne Doudna",
@@ -160,7 +160,7 @@ let cientistas = [
         morte: "Dias atuais",
         wiki: "https://pt.wikipedia.org/wiki/Jennifer_Doudna",
         img: "img/cientistas/jennifer-anne.jpg",
-        tags: "biologia quimica bioquimica química genetica"
+        tags: "biologia quimica bioquimica química genetica jenifer ane doudna saude saúde"
     },
     {
         nome: "Joy Buolamwini",
@@ -181,6 +181,56 @@ let cientistas = [
         wiki: "https://movimentomulheresnati.labbs.com.br/carol-shaw/",
         img: "img/cientistas/carol-shaw.jpg",
         tags: "computacao computacão computaçao programação programaçao programacao tech tecnologia tecnologica tecnológica eletronico jogo videogame game video industria desenvolvedora"
+    },
+    {
+        nome: "Emmy Noether",
+        descricao: "Matemática alemã que fez contribuições fundamentais para a álgebra abstrata e a física teórica. Seus trabalhos sobre anéis, corpos e álgebras são considerados marcos na história da matemática.",
+        area: "Matemática",
+        nascimento: "1882",
+        morte: "1935",
+        wiki: "https://en.wikipedia.org/wiki/Emmy_Noether",
+        img: "img/cientistas/emmy-noether.webp",
+        tags: "matematica algebra abstrata fisica teorica teoria algebra"
+    },
+    {
+        nome: "Irène Joliot-Curie",
+        descricao: "Física e química francesa, filha de Marie e Pierre Curie. Recebeu o Prêmio Nobel de Química em 1935, junto com seu marido Frédéric Joliot-Curie, por suas descobertas sobre a radioatividade artificial.",
+        area: "Física e Química",
+        nascimento: "1897",
+        morte: "1956",
+        wiki: "https://en.wikipedia.org/wiki/Irène_Joliot-Curie",
+        img: "img/cientistas/irene-curie.jpg",
+        tags: "fisica quimica quimica fisica radioatividade nobel premio nobeu currie irene iréne joliot"
+    },
+    {
+        nome: "Maria Goeppert-Mayer",
+        descricao: "Físico-química teórica alemã-americana, conhecida por seu trabalho no modelo nuclear de camadas, pelo qual recebeu o Prêmio Nobel de Física em 1963. Explorou a organização dos prótons e nêutrons dentro do núcleo atômico, propondo que eles se organizam em camadas, similar aos elétrons em um átomo. Essa descoberta revolucionou a compreensão da física nuclear.",
+        area: "Física e Química",
+        nascimento: "1906",
+        morte: "1972",
+        wiki: "https://en.wikipedia.org/wiki/Maria_Goeppert_Mayer",
+        img: "img/cientistas/maria-goeppert.webp",
+        tags: "fisica quimica fisica-quimica premio nobel nobeu maria goeppert mayer"
+    },
+    {
+        nome: "Dorothy Hodgkin",
+        descricao: "Química britânica, pioneira no uso da cristalografia de raios X para determinar a estrutura de biomoléculas complexas, como a penicilina, a insulina e a vitamina B12. Ganhadora do Prêmio Nobel de Química em 1964.",
+        area: "Química",
+        nascimento: "1910",
+        morte: "1994",
+        wiki: "https://en.wikipedia.org/wiki/Dorothy_Hodgkin",
+        img: "img/cientistas/dorothy-hodgkin.jpg",
+        tags: "quimica raios-x bioquimica biomolecula biomoleculas vitaminas b12 b 12 premio nobel nobeu medicina saude saúde"
+    },
+    {
+        nome: "Johanna Döbereiner",
+        descricao: "Engenheira agrônoma brasileira, pioneira em biologia do solo. Seus estudos sobre a fixação biológica de nitrogênio em leguminosas foram fundamentais para o desenvolvimento da agricultura sustentável no Brasil.",
+        area: "Agronomia e Biologia do Solo",
+        nascimento: "1924",
+        morte: "2000",
+        wiki: "https://pt.wikipedia.org/wiki/Johanna_D%C3%B6bereiner",
+        img: "img/cientistas/johanna.webp",
+        tags: "agronomia agronoma engenharia biologica biológica biológicas biologicas agro de nitrogenio agricultura sustentavel brasil johanna dobereiner"
     }
 ];
 
